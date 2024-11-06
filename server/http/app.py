67c012f560f9d441a36bfcc8a260aed6ba049143
Noé Steiner <72690981+unshade@users.noreply.github.com>
2024-11-06 21:02:49 +0100

    FEAT : scapy listener

--- server/http/app.py
+++ server/http/app.py
@@ -1,35 +1,47 @@
-from flask import Flask, request
-import json
-import os
-from datetime import datetime
+from scapy.all import *
+from scapy.layers.http import HTTP
+from scapy.layers.inet import TCP, IP
 
-app = Flask(__name__)
-
+# Create directory for storing captured packets
 resources_dir = os.path.join(os.getcwd(), 'resources/http')
 if not os.path.exists(resources_dir):
     os.makedirs(resources_dir)
 
-@app.before_request
-def log_request():
-    request_data = {
-        'method': request.method,
-        'path': request.path,
-        'headers': dict(request.headers),
-        'body': request.get_data(as_text=True) if request.data else None,
-        'GET_params': request.args.to_dict(),
-        'POST_params': request.form.to_dict(),
-    }
 
-    timestamp = datetime.now().strftime('%Y%m%d_%H%M%S_%f')
-    file_name = f"request_{timestamp}.json"
-    file_path = os.path.join(resources_dir, file_name)
+def packet_callback(pak):
+    if pak.haslayer(HTTP):
+        timestamp = datetime.now().strftime('%Y%m%d_%H%M%S_%f')
+        file_name = f"packet_{timestamp}.json"
+        file_path = os.path.join(resources_dir, file_name)
 
-    with open(file_path, 'w', encoding='utf-8') as f:
-        json.dump(request_data, f, ensure_ascii=False, indent=4)
+        print(pak)
+        packet_data = {
+            'source_ip': pak[IP].src,
+            'destination_ip': pak[IP].dst,
+            'source_port': pak[TCP].sport,
+            'destination_port': pak[TCP].dport,
+            #'method': pak[HTTP].Method.decode() if pak[HTTP].Method else None,
+            #'path': pak[HTTP].Path.decode() if pak[HTTP].Path else None,
+            'headers': dict(pak[HTTP].fields),
+        }
 
-@app.route('/status')
-def index():
-    return "Running"
+        # Try to decode the payload if it exists
+        if pak.haslayer(Raw):
+            try:
+                packet_data['body'] = pak[Raw].load.decode()
+            except UnicodeDecodeError:
+                packet_data['body'] = "Unable to decode payload"
+
+        with open(file_path, 'w', encoding='utf-8') as f:
+            json.dump(packet_data, f, ensure_ascii=False, indent=4)
+
+        print(f"Captured HTTP packet: {file_name}")
+
+
+def start_capture():
+    print("Starting packet capture... Press Ctrl+C to stop.")
+    sniff(filter="tcp port 80", prn=packet_callback, store=0)
+
 
 if __name__ == '__main__':
-    app.run(debug=True, port=80, host='0.0.0.0')
+    start_capture()
