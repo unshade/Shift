fbc1dd387bd0af75d2c4b9ac04728c8bf644d03a
LimpSword <contact@limpsword.fr>
2025-01-27 16:59:11 +0100

    separate cookies headers with § for now

--- server/proto/http/request_service.py
+++ server/proto/http/request_service.py
@@ -9,7 +9,7 @@ def decode_headers(headers):
             v = decode_headers(v)
         # If this is a Cookie and there is already a Set-Cookie header, append the new value
         if 'cookie' in k.lower() and k in decoded_headers:
-            decoded_headers[k] += f"; {v}"
+            decoded_headers[k] += f"§ {v}"
         else:
             decoded_headers[k] = v
     return decoded_headers
